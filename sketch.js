72a4fbb22d5cac425a45b9a193bb77f634ba83da
Dillon Brock <dillonbrock@Dillons-MacBook-Pro.local>
2021-01-14 16:55:49 -0800

    made gradient from white to black top to bottom

--- sketch.js
+++ sketch.js
@@ -1,26 +1,46 @@
 const width = 1000;
 const height = 1000;
 
+let lowerLines = [];
+let upperLines = [];
+
 function setup() {
 
   createCanvas(width, height)
   background(0);
+
+  for (var i = 0; i < 1000; i++) {
+    lowerLines[i] = new LowerGradient(i);
+    upperLines[i] = new UpperGradient(i);
+  } 
 }
 
 
 
 function draw() {
 
-  // noStroke();
-  // fill(255);
-  // beginShape();
-  // vertex(0, height*0.1);
-  // vertex(width, );
-  // vertex(width, height);
-  // vertex(width, height/2);
-  // endShape();
+  for (var i = 0; i < 1000; i++) {
+    strokeWeight(1);
+    stroke((1000-i)*(255/1000));
+    line(0, i, 1000, i);
+  }
+
+  // for (var i = 0; i < lowerLines.length; i++) {
+  //   lowerLines[i].display();
+  // }
+  // for (var i = 0; i < upperLines.length; i++) {
+  //   upperLines[i].display();
+  // }
 
   
+}
+
+class generalGradient {
+
+  constructor() {
+
+
+  }
 }
 
 class LowerGradient {
@@ -29,7 +49,7 @@ class LowerGradient {
 
     this.x1 = num;
     this.x2 = num;
-    this.y1 = 1000 - ((1000 - num) * tan(PI/4));
+    this.y1 = num;
     this.y2 = height;
     this.lineColor = num * (255/1000);
 
@@ -38,7 +58,7 @@ class LowerGradient {
   display() {
     strokeWeight(1);
     stroke(this.lineColor);
-    line(this.x1, this.x2, this.y1, this.y2);
+    line(this.x1, this.y1, this.x2, this.y2);
   }
 }
 
@@ -46,6 +66,18 @@ class UpperGradient {
 
   constructor(num) {
 
+    this.x1 = num;
+    this.x2 = num;
+    this.y1 = 0;
+    this.y2 = num ;
+    this.lineColor = (1000 - num) * (255/1000);
 
+  }
+
+  display() {
+
+    strokeWeight(1);
+    stroke(this.lineColor);
+    line(this.x1, this.y1, this.x2, this.y2);
   }
 }
