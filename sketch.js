be29e3e9d7b427d768ac0edab4cdbea517eee601
Dillon Brock <dillonbrock@Dillons-MacBook-Pro.local>
2021-01-15 10:45:11 -0800

    made peaks w gradient white to black top to bottom and black background

--- sketch.js
+++ sketch.js
@@ -1,28 +1,22 @@
 const width = 1000;
 const height = 1000;
 
-let lowerLines = [];
-let upperLines = [];
 
 function setup() {
 
   createCanvas(width, height)
   background(0);
 
-  for (var i = 0; i < 1000; i++) {
-    lowerLines[i] = new LowerGradient(i);
-    upperLines[i] = new UpperGradient(i);
-  } 
 }
 
 
 
 function draw() {
 
-  for (var i = 0; i < 1000; i++) {
+  for (var i = 0; i < 800; i++) {
     strokeWeight(1);
-    stroke((1000-i)*(255/1000));
-    line(0, i, 1000, i);
+    stroke(i*(255/800));
+    line(0, 1000-i, 1000, 1000-i);
   }
 
   // for (var i = 0; i < lowerLines.length; i++) {
@@ -32,52 +26,17 @@ function draw() {
   //   upperLines[i].display();
   // }
 
-  
-}
+  fill(0);
+  noStroke();
+  beginShape();
+  vertex(0,0);
+  vertex(0, 0.7*height);
+  vertex(0.2*width, 0.3*height);
+  vertex(0.4*width, 0.5*height);
+  vertex(0.6*width, 0.2*height);
+  vertex(0.8*width, 0.6*height);
+  vertex(width, 0.25*height);
+  vertex(width, 0);
+  endShape();
 
-class generalGradient {
-
-  constructor() {
-
-
-  }
-}
-
-class LowerGradient {
-
-  constructor(num) {
-
-    this.x1 = num;
-    this.x2 = num;
-    this.y1 = num;
-    this.y2 = height;
-    this.lineColor = num * (255/1000);
-
-  }
-
-  display() {
-    strokeWeight(1);
-    stroke(this.lineColor);
-    line(this.x1, this.y1, this.x2, this.y2);
-  }
-}
-
-class UpperGradient {
-
-  constructor(num) {
-
-    this.x1 = num;
-    this.x2 = num;
-    this.y1 = 0;
-    this.y2 = num ;
-    this.lineColor = (1000 - num) * (255/1000);
-
-  }
-
-  display() {
-
-    strokeWeight(1);
-    stroke(this.lineColor);
-    line(this.x1, this.y1, this.x2, this.y2);
-  }
 }
