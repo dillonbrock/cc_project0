a01ab6c5508c45e778ec3180e4c362d1465dc0a1
Dillon Brock <dillonbrock@Dillons-MBP.hsd1.ma.comcast.net>
2021-01-19 10:14:41 -0500

    fixed maxheight logic, need to find algorithm for drawing vertices

--- sketch.js
+++ sketch.js
@@ -2,24 +2,28 @@ const width = 1000;
 const height = 1000;
 let peakHeights = [];
 let indices = [];
-let maxPeakHeight = random(0.95);
-let vertices = floor(random(11));
+let peakHeights = [];
+let maxPeakHeight = 0.3;
+let vertices;
 
 function setup() {
 
   createCanvas(width, height)
   background(0);
+  vertices = floor(random(5, 13));
   while (vertices % 2 == 0) {
-    vertices = floor(random(11));
+    vertices = floor(random(5, 13));
   }
   for (var i = 0; i < vertices; i++) {
-    if (i == 0) {
-      peakHeights[i] = maxPeakHeight;
+    peakHeights[i] = random(0.3, 0.95);
+  }
+  for (var i = 0; i < peakHeights.length; i++) {
+    if (peakHeights[i] <= maxPeakHeight) {
+
     } else {
-      peakHeights[i] = random(0.15, maxPeakHeight);
+      maxPeakHeight = peakHeights[i];
     }
   }
-
 }
 
 
@@ -32,43 +36,37 @@ function draw() {
     line(0, 1000-i, 1000, 1000-i);
   }
 
-  // for (var i = 0; i < lowerLines.length; i++) {
-  //   lowerLines[i].display();
-  // }
-  // for (var i = 0; i < upperLines.length; i++) {
-  //   upperLines[i].display();
-  // }
+  // // for (var i = 0; i < lowerLines.length; i++) {
+  // //   lowerLines[i].display();
+  // // }
+  // // for (var i = 0; i < upperLines.length; i++) {
+  // //   upperLines[i].display();
+  // // }
 
-  // fill(0);
-  // noStroke();
-  // beginShape();
-  // vertex(0,0);
-  // vertex(0, 0.7*height);
-  // vertex(0.2*width, 0.3*height);
-  // vertex(0.4*width, 0.5*height);
-  // vertex(0.6*width, 0.2*height);
-  // vertex(0.8*width, 0.6*height);
-  // vertex(width, 0.25*height);
-  // vertex(width, 0);
-  // endShape();
+  // // fill(0);
+  // // noStroke();
+  // // beginShape();
+  // // vertex(0,0);
+  // // vertex(0, 0.7*height);
+  // // vertex(0.2*width, 0.3*height);
+  // // vertex(0.4*width, 0.5*height);
+  // // vertex(0.6*width, 0.2*height);
+  // // vertex(0.8*width, 0.6*height);
+  // // vertex(width, 0.25*height);
+  // // vertex(width, 0);
+  // // endShape();
 
-  drawMountains(vertices, maxPeakHeight);
+  // drawMountains(vertices, maxPeakHeight);
 
 }
 
-function drawMountains(numOfVertices, maxHeight) {
+function drawMountains(numOfVertices) {
 
-  let heightPicker = floor(random(numOfVertices))
   fill(0);
   noStroke();
   beginShape();
   vertex(0,0);
-  for (var i = 0; i < numOfVertices; i++) {
-    while (indices.contains(heightPicker)) {
-      heightPicker = floor(random(numOfVertices));
-    }
-    vertex(i * (width/numOfVertices), (height-peakHeights[heightPicker]) * height);
-  }
+
   vertex(width, 0);
   endShape();
 
