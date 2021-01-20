589ab55feb63f4e1f51fd5fcdf47e68610df69e3
Dillon Brock <dillonbrock@Dillons-MBP.hsd1.ma.comcast.net>
2021-01-19 21:36:42 -0500

    made switch case function for two peak gradient, now I need to do the logic for more than two peaks

--- sketch.js
+++ sketch.js
@@ -9,12 +9,16 @@ let allHeights = [];
 let vertices;
 let orderedPeakHeights = [];
 let orderedValleyHeights = [];
+let numberedValleys = [];
+let valleyNumber;
+let numberedPeaks = [];
 
 function setup() {
 
   createCanvas(width, height)
   noLoop();
   background(0);
+  console.log(Number('012'));
   vertices = floor(random(5, 10));
   while (vertices % 2 == 0) {
     vertices = floor(random(5, 10));
@@ -26,19 +30,13 @@ function setup() {
     valleyHeights[i] = random(0.2, 0.5);
   }
   for (var i = 0; i < peakHeights.length; i++) {
-    if (peakHeights[i] <= maxPeakHeight) {
-
-    } else {
+    if (peakHeights[i] > maxPeakHeight) {
       maxPeakHeight = peakHeights[i];
     }
-    if (peakHeights[i] >= minValleyHeight) {
-
-    } else {
+    if (peakHeights[i] < minValleyHeight) {
       minValleyHeight = peakHeights[i];
     }
-    if (peakHeights[i] >= minPeakHeight) {
-
-    } else {
+    if (peakHeights[i] < minPeakHeight) {
       minPeakHeight = peakHeights[i];
     }
   }
@@ -69,6 +67,14 @@ function setup() {
       }
       orderedValleyHeights.push(minVal);
     }
+    for (var i = 0; i < valleyHeights.length; i++) {
+      numberedValleys.push(orderedValleyHeights.indexOf(valleyHeights[i]));
+    }
+    for (var i = 0; i < peakHeights.length; i++) {
+      numberedPeaks.push(orderedPeakHeights.indexOf(peakHeights[i]));
+    }
+    valleyNumber = Number(numberedValleys.join(''));
+
 }
 
 
@@ -84,6 +90,8 @@ function draw() {
   drawBackground(vertices);
 
 }
+
+
 
 function drawBackground(numOfVertices) {
 
@@ -118,31 +126,21 @@ function drawBackground(numOfVertices) {
         line(findxOffset(orderedPeakHeights[1], 1, i), i, findxOffset(orderedPeakHeights[0], -1, i), i);
         line(findxOffset(orderedPeakHeights[0], 1, i), i, width, i);
         if (peakHeights.length == 2) {
-          //shallowest valley to deepest and shortest peak to tallest
-          if(valleyHeights.indexOf(orderedValleyHeights[2]) < valleyHeights.indexOf(orderedValleyHeights[1]) && valleyHeights.indexOf(orderedValleyHeights[1]) < valleyHeights.indexOf(orderedValleyHeights[0])) {
-            // hasn't reached shallowest valley yet
-            if (i < height*(1-orderedValleyHeights[2])) {
-              line(0, i, findxOffset(orderedPeakHeights[1], -1, i), i);
-              line(findxOffset(orderedPeakHeights[1], 1, i), i, findxOffset(orderedPeakHeights[0], -1, i), i);
-              line(findxOffset(orderedPeakHeights[0], 1, i), i, width, i);
-            } else if (i < height*(1-orderedValleyHeights[1])) {
-                line(findxOffset(orderedPeakHeights[1], 1, i), i, findxOffset(orderedPeakHeights[0], -1, i), i);
-                line(findxOffset(orderedPeakHeights[0], 1, i), i, width, i);
-            } else {
-              line(findxOffset(orderedPeakHeights[0], 1, i), i, width, i);
-            }
-          } else if {
-
-          }
+          twoPeakSwitchCase(valleyNumber, i);
         }
       } else {    // tallest peak comes first
         line(0, i, findxOffset(orderedPeakHeights[0], -1, i), i);
         line(findxOffset(orderedPeakHeights[0], 1, i), i, findxOffset(orderedPeakHeights[1], -1, i), i);
         line(findxOffset(orderedPeakHeights[1], 1, i), i, width, i);
+        if (peakHeights.length == 2) {
+          twoPeakSwitchCase(valleyNumber, i);
+        }
       }
     }
   }
 }
+
+
 
 
 function findxOffset(peakHeight, side, num) {     //side is -1 (left) or +1 (right)
@@ -153,13 +151,75 @@ function findxOffset(peakHeight, side, num) {     //side is -1 (left) or +1 (rig
   }
 }
 
-function drawUpperGradient(numOfPeaks) {
-  for (var i = 0; i < height * (1-minValleyHeight); i++) {
-    strokeWeight(1);
-    stroke(255-(i*255/(height*(1-minValleyHeight))));
-    if (i < height * (1-maxPeakHeight)) {
-      line(0, i, 1000, i);
-    }
+function twoPeakSwitchCase(valleyNum, itr) {
+
+  if (itr < height*(1-orderedValleyHeights[2])) {
+    line(0, itr, findxOffset(orderedPeakHeights[numberedPeaks[0]], -1, itr), itr);
+    line(findxOffset(orderedPeakHeights[numberedPeaks[0]], 1, itr), itr, findxOffset(orderedPeakHeights[numberedPeaks[1]], -1, itr), itr);
+    line(findxOffset(orderedPeakHeights[numberedPeaks[1]], 1, itr), itr, width, itr);
+  }
+  else { 
+    switch (valleyNum) {
+  // shallowest to deepest valley
+      case 210:  
+        if (itr < height*(1-orderedValleyHeights[1])) {
+          line(findxOffset(orderedPeakHeights[numberedPeaks[0]], 1, itr), itr, findxOffset(orderedPeakHeights[numberedPeaks[1]], -1, itr), itr);
+          line(findxOffset(orderedPeakHeights[numberedPeaks[1]], 1, itr), itr, width, itr);
+        } 
+        else {
+          line(findxOffset(orderedPeakHeights[numberedPeaks[1]], 1, itr), itr, width, itr);
+        }
+      break;
+      //deepest to shallowest valley
+      case 12:
+        if(itr < height*(1-orderedValleyHeights[1])) {
+          line(0, itr, findxOffset(orderedPeakHeights[numberedPeaks[0]], -1, itr), itr);
+          line(findxOffset(orderedPeakHeights[numberedPeaks[0]], 1, itr), itr, findxOffset(orderedPeakHeights[numberedPeaks[1]], -1, itr), itr);
+        }
+        else{
+          line(0, itr, findxOffset(orderedPeakHeights[numberedPeaks[0]]), itr);
+        }
+      break;
+      //deepest, shallowest, medium
+      case 21:
+        if (itr < height * (1-orderedValleyHeights[1])) {
+          line(0, itr, findxOffset(orderedPeakHeights[numberedPeaks[0]], -1, itr), itr);
+          line(findxOffset(orderedPeakHeights[numberedPeaks[1]], 1, itr), itr, width, itr);
+        }
+        else {
+          line(0, itr, findxOffset(orderedPeakHeights[numberedPeaks[0]], -1, itr), itr);
+        }
+      break;
+      //shallowest, deepest, medium
+      case 201:
+        if (itr < height * (1-orderedValleyHeights[1])) {
+          line(findxOffset(orderedPeakHeights[numberedPeaks[0]], 1, itr), itr, findxOffset(orderedPeakHeights[numberedPeaks[1]], -1, itr), itr);
+          line(findxOffset(orderedPeakHeights[numberedPeaks[1]], 1, itr), itr, width, itr);
+        }
+        else {
+          line(findxOffset(orderedPeakHeights[numberedPeaks[0]], 1, itr), itr, findxOffset(orderedPeakHeights[numberedPeaks[1]], -1, itr), itr);
+        }
+      break;
+      //medium, deepest, shallowest
+      case 102:
+        if (itr < height * (1-orderedValleyHeights[1])) {
+          line(0, itr, findxOffset(orderedPeakHeights[numberedPeaks[0]], -1, itr), itr);
+          line(findxOffset(orderedPeakHeights[numberedPeaks[0]], 1, itr), itr, findxOffset(orderedPeakHeights[numberedPeaks[1]], -1, itr), itr);
+        }
+        else {
+          line(findxOffset(orderedPeakHeights[numberedPeaks[0]], 1, itr), itr, findxOffset(orderedPeakHeights[numberedPeaks[1]], -1, itr), itr);
+        }
+      break;
+      //medium, shallowest, deepest
+      case 120:
+        if (itr < height * (1-orderedValleyHeights[1])) {
+          line(0, itr, findxOffset(orderedPeakHeights[numberedPeaks[0]], -1, itr), itr);
+          line(findxOffset(orderedPeakHeights[numberedPeaks[1]], 1, itr), itr, width, itr);
+        }
+        else {
+          line(findxOffset(orderedPeakHeights[numberedPeaks[1]], 1, itr), itr, width, itr);
+        }
+    }        
   }
 }
 
